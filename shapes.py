72e59d68c87930697e74588beff8300b5ac31137
Tessel Haagen <xelfjex@gmail.com>
2024-12-13 11:43:07 +0100

    Create shapes.py

---
+++ shapes.py
@@ -0,0 +1,64 @@
+import math
+
+class Shape():
+    def __init__(self, width):
+        self.width = width
+        self.perimeter = 0 #self.calc_perimeter()
+        self.surface = 0 #self.calc_surface()
+    
+    # def calc_perimeter(self):
+    #     raise NotImplementedError
+    
+    # def calc_surface(self):
+    #     raise NotImplementedError
+
+    def __eq__(self, other):
+        return self.surface == other.surface
+
+    def __lt__(self, other):
+        return self.surface < other.surface
+    
+    def __str__(self):
+        return f'Shape with width of {self.width}'
+    
+
+
+class Circle(Shape):
+    def __init__(self, width):
+        super().__init__(width)
+        self.surface = self.calc_surface()
+        self.perimeter = self.calc_perimeter()
+    
+    def calc_perimeter(self):
+        return math.pi * self.width
+    
+    def calc_surface(self):
+        return math.pi * ((self.width/2) ** 2)
+    
+    def __str__(self):
+        return f'Circle with width of {self.width}'
+    
+
+
+class Square(Shape):
+    def __init__(self, width):
+        super().__init__(width)
+        self.surface = self.calc_surface()
+        self.perimeter = self.calc_perimeter()
+
+    def calc_perimeter(self):
+        return 4 * self.width
+    
+    def calc_surface(self):
+        return self.width * self.width # self.width ** 2
+
+
+
+circle = Circle(12)
+square = Square(10)
+
+print(circle > square)
+print(circle.surface - square.surface)
+
+print(circle)
+print(square)
